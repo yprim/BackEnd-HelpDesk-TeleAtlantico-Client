5ba8c75acf2c168bdd03ab070aece49ea3adaa95
Yoselyn Priscilla <priscipo@gmail.com>
2021-02-21 22:46:10 -0600

    cambios

--- client/src/main/java/com/example/client/service/IssueService.java
+++ client/src/main/java/com/example/client/service/IssueService.java
@@ -20,4 +20,9 @@ public class IssueService {
     public void save(Issue issue){repository.save(issue);}
     public Issue get (int id){ return repository.findById(id).get();}
     public void delete(int id){repository.deleteById(id);}
+
+    public Issue getByClientId(int id){
+        return repository.findAll().get();
+    }
+
 }
